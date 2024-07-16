3811e6f10433baf59ebcaf05d988b18d5b080f5a
Alessio Buccino <alejoe9187@gmail.com>
2024-07-16 18:57:58 +0200

    Fix add_devices_from_rig

--- code/utils.py
+++ code/utils.py
@@ -11,7 +11,7 @@ import pynwb
 from pynwb.file import Device
 
 
-def get_devices_from_rig_metadata(session_folder: str, segment_index: int=0):
+def get_devices_from_rig_metadata(session_folder: str, segment_index: int = 0):
     """
     Return NWB devices from metadata target locations.
 
@@ -51,79 +51,137 @@ def get_devices_from_rig_metadata(session_folder: str, segment_index: int=0):
 
     data_streams = None
     if session is not None:
-        if "schema_version" in session:
-            session_schema_version = session["schema_version"]
-            if parse(session_schema_version) >= parse("0.3.0"):
-                data_streams = session["data_streams"]
-            else:
-                warnings.warn(f"v{session_schema_version} for session schema is not currently supported")
+        session_schema_version = session.get("schema_version", None)
 
-    # Add devices here
-    devices = None
-    devices_target_location = {}
-    if rig is not None and data_streams is not None:
-        if "schema_version" in rig:
-            rig_schema_version = rig["schema_version"]
-            if parse(rig_schema_version) >= parse("0.5.1"):
-                if data_streams is not None:
-                    probes_in_session = session["data_streams"][segment_index]["probes"]
-                    ephys_modules = rig["ephys_modules"]
+        if session_schema_version is None:
+            warnings.warn(f"Session file does not have schema_version")
+            return devices, devices_target_location
+        if parse(session_schema_version) >= parse("0.3.0"):
+            data_streams = session.get("data_streams", None)
+            if data_streams is None:
+                warnings.warn(f"Session file does not have data_streams")
+                return None, None
+        else:
+            warnings.warn(f"v{session_schema_version} for session schema is not currently supported")
+            return None, None
+    else:
+        warnings.warn(f"Session file not found in {session_folder}")
+        return None, None
 
-                    if len(probes_in_session) <= len(ephys_modules):
-                        for probe in probes_in_session:
-                            probe_name = probe["name"]
-                            for ephys_module in ephys_modules:
-                                probe_info = ephys_module["probes"][0]
-                                if probe_info["name"] == probe_name:
-                                    probe_device_name = probe_info["name"]
-                                    probe_device_description = f"Model: {probe_info['probe_model']} - Serial number: {probe_info['serial_number']}"
-                                    probe_device_manufacturer = f"{probe_info['manufacturer']}"
-                                    probe_device = Device(
-                                        name=probe_device_name,
-                                        description=probe_device_description,
-                                        manufacturer=probe_device_manufacturer,
-                                    )
-                                    if devices is None:
-                                        devices = {}
-                                    devices[probe_device_name] = probe_device
-                                    devices_target_location[probe_device_name] = probe["primary_targeted_structure"]
+    stimulus_epochs = session.get("stimulus_epochs", None)
+    if stimulus_epochs is not None:
+        stimulus_device_names = []
+        for epoch in stimulus_epochs:
+            stimulus_device_names += epoch.get("stimulus_device_names", [])
 
-                                    # Add internal lasers for NP-opto
-                                    if "lasers" in probe_info and len(probe_info["lasers"]) > 1:
-                                        for laser in probe_info["lasers"]:
-                                            laser_device_name = laser["name"]
-                                            laser_device_description = f"Type: internal - Wavelength: {laser['wavelength']}nm - Max power: {laser['maximum_power']}uW - Coupling: {laser['coupling']}"
-                                            laser_device_manufacturer = laser["manufacturer"]
-                                            internal_laser_device = Device(
-                                                name=laser_device_name,
-                                                description=laser_device_description,
-                                                manufacturer=laser_device_manufacturer,
-                                            )
-                                            if devices is None:
-                                                devices = {}
-                                            devices[laser_device_name] = internal_laser_device
+    if rig is not None:
+        rig_schema_version = rig.get("schema_version", None)
+        if rig_schema_version is None:
+            warnings.warn(f"Rig file does not have schema_version")
+            return None, None
+        if parse(rig_schema_version) >= parse("0.5.1"):
+            ephys_modules = session["data_streams"][segment_index]["ephys_modules"]
+            ephys_assemblies = rig.get("ephys_assemblies", [])
+            laser_assemblies = rig.get("laser_assemblies", [])
 
-                    # # TODO: Add external lasers should be added by other capsules
-                    # if "laser_modules" in rig:
-                    #     for laser in rig["laser_modules"][0]["lasers"]:
-                    #         laser_device_name = laser["name"]
-                    #         laser_device_description = f"Type: external - Wavelength: {laser['wavelength']}nm"
-                    #         if "coupling" in laser:
-                    #             laser_device_description += f" - Coupling: {laser['coupling']}"
-                    #         laser_device_manufacturer = laser['manufacturer'] # TODO get this info from rig.json
-                    #         external_laser_device = Device(name=laser_device_name,
-                    #                                        description=laser_device_description,
-                    #                                        manufacturer=laser_device_manufacturer)
-                    #       if added_devices is None:
-                    #           added_devices = {}
-                    #       added_devices[laser_device_name] = internal_laser_device
+            # Get probes from session
+            devices = {}
+            devices_target_location = {}
+            laser_devices = {}
+            for ephys_module in ephys_modules:
+                assembly_name = ephys_module["assembly_name"]
+                ephys_assemblies_by_name = [
+                    assembly for assembly in ephys_assemblies if assembly["name"] == assembly_name
+                ]
+                if len(ephys_assemblies_by_name) == 0:
+                    warnings.warn(f"Assembly {assembly_name} not found in rig.json")
+                    continue
+                ephys_assembly = ephys_assemblies_by_name[0]
+                probes_in_assembly = ephys_assembly["probes"]
 
-            #                     else:
-            #                         warnings.warn(f"Inconsistency between rig and session devices")
-            else:
-                warnings.warn(f"v{rig_schema_version} for rig schema is not currently supported")
+                for probe_info in probes_in_assembly:
+                    probe_device_name = probe_info["name"]
+                    probe_model_name = probe_info.get("probe_model", None)
+                    probe_device_manufacturer = probe_info.get("manufacturer", None)
+                    probe_serial_number = probe_info.get("serial_number", None)
+                    probe_device_description = ""
+                    if probe_device_name is None:
+                        if probe_model_name is not None:
+                            probe_device_name = probe_model_name
+                        else:
+                            probe_device_name = "Probe"
+                    if probe_model_name is not None:
+                        probe_device_description += f"Model: {probe_device_description}"
+                    if probe_serial_number is not None:
+                        if len(probe_device_description) > 0:
+                            probe_device_description += " - "
+                        probe_device_description += f"Serial number: {probe_serial_number}"
+                    probe_device = Device(
+                        name=probe_device_name,
+                        description=probe_device_description,
+                        manufacturer=probe_device_manufacturer,
+                    )
+                    if probe_device_name not in devices:
+                        devices[probe_device_name] = probe_device
+                        devices_target_location[probe_device_name] = ephys_module["primary_targeted_structure"]
+
+                    # Add internal lasers for NP-opto
+                    if "lasers" in probe_info and len(probe_info["lasers"]) > 1:
+                        for laser in probe_info["lasers"]:
+                            laser_device_name = laser["name"]
+                            laser_device_description, laser_device_manufacturer = get_laser_description_manufacturer(
+                                laser, "internal"
+                            )
+                            internal_laser_device = Device(
+                                name=laser_device_name,
+                                description=laser_device_description,
+                                manufacturer=laser_device_manufacturer,
+                            )
+                            if laser_device_name not in laser_devices:
+                                laser_devices[laser_device_name] = internal_laser_device
+
+            for laser_assembly in laser_assemblies:
+                for laser in laser_assembly["lasers"]:
+                    laser_device_name = laser["name"]
+                    laser_device_description, laser_device_manufacturer = get_laser_description_manufacturer(
+                        laser, "external"
+                    )
+                    external_laser_device = Device(
+                        name=laser_device_name,
+                        description=laser_device_description,
+                        manufacturer=laser_device_manufacturer,
+                    )
+                    if laser_device_name not in laser_devices:
+                        laser_devices[laser_device_name] = external_laser_device
+
+            # only add used lasers
+            if len(stimulus_device_names) > 0:
+                for stimulus_device_name in stimulus_device_names:
+                    if stimulus_device_name in laser_devices:
+                        devices[stimulus_device_name] = laser_devices[stimulus_device_name]
+        else:
+            warnings.warn(f"v{rig_schema_version} for rig schema is not currently supported")
+    else:
+        warnings.warn(f"Rig file not found in {session_folder}")
 
     return devices, devices_target_location
+
+
+def get_laser_description_manufacturer(laser, type):
+    laser_device_description = f"Type: {type} "
+    wavelength = laser.get("wavelength", None)
+    if wavelength is not None:
+        laser_device_description += f" - Wavelength: {wavelength} {laser.get('wavelength_unit', 'nanometer')}"
+    max_power = laser.get("maximum_power", None)
+    if max_power is not None:
+        laser_device_description += f" - Max power: {max_power} {laser.get('power_unit', 'milliwatt')}"
+    coupling = laser.get("coupling", None)
+    if coupling is not None:
+        laser_device_description += f" - Coupling: {coupling}"
+    laser_device_manufacturer = laser.get("manufacturer", None)
+    if isinstance(laser_device_manufacturer, dict):
+        laser_device_manufacturer = laser_device_manufacturer.get("name", None)
+    return laser_device_description, laser_device_manufacturer
 
 
 def add_waveforms_with_uneven_channels(
@@ -147,7 +205,7 @@ def add_waveforms_with_uneven_channels(
     from neuroconv.tools.spikeinterface.spikeinterface import (
         add_electrodes_info,
         add_units_table,
-        get_electrode_group_indices
+        get_electrode_group_indices,
     )
 
     # TODO: move into add_units
@@ -157,7 +215,7 @@ def add_waveforms_with_uneven_channels(
     ], f"Argument write_as ({write_as}) should be one of 'units' or 'processing'!"
     if write_as == "units":
         assert units_name == "units", "When writing to the nwbfile.units table, the name of the table must be 'units'!"
-    write_in_processing_module = False if write_as == "units" else True    
+    write_in_processing_module = False if write_as == "units" else True
 
     num_units = len(waveform_extractor.unit_ids)
     # pad with zeros if needed
