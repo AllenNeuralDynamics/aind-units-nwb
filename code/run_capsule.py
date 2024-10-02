4eb5537e6e762bc4a07c958b6b1da76799776825
Alessio Buccino <alessio.buccino@alleninstitute.org>
2024-10-02 17:04:38 +0000

    Set load_extensions to False when loading analyzer

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -303,7 +303,7 @@ if __name__ == "__main__":
                                 # binary format
                                 analyzer_folder = postprocessed_folder / recording_name
 
-                            analyzer = si.load_sorting_analyzer(analyzer_folder)
+                            analyzer = si.load_sorting_analyzer(analyzer_folder, load_extensions=False)
 
                             # Load curated sorting and set properties
                             sorting_curated = si.load_extractor(curated_folder / recording_name)
